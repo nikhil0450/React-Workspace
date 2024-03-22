a746e6b18084021555da11f2e91ca4d44b00d402
nikhil0450 <nikhilkulkarni2208@gmail.com>
2024-03-22 13:39:26 +0530

    workspace

---
+++ react1/README.md
@@ -0,0 +1,6 @@
+# Fetch Data from JSON & render in React
+
+- We have a dummy json data URL, from that URL we will be fetching the objects data as per our need
+- React + Vite 
+
+
---
+++ react1/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+const MyComponent = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://dummyjson.com/products');
+        const jsonData = await response.json();
+        setProducts(jsonData.products);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Product Titles:</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MyComponent;
